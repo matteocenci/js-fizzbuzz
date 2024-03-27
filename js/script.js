c89516fadaabf648dc72f08247d1ab6e57e2d4a8
matteocenci <matteocenci01@gmail.com>
2024-03-27 17:09:35 +0100

    feat: output result works and exercise done

--- js/script.js
+++ js/script.js
@@ -1,35 +1,37 @@
 const rowElem = document.querySelector(".row");
 console.log(rowElem);
+// let fizzBuzzBox = "FizzBuzz";
+// let fizzBox = "Fizz";
+// let buzzBox ="Buzz"
 
 
 
 
 
-
-for (let i = 0; i<=100; i++) {
+for (let i = 1; i<=100; i++) {
     
     let bgClass;
+    let txtResult;
     if(i % 15 === 0) {
         console.log("FizzBuzz");
+        txtResult = "FizzBuzz";
         bgClass = "fizzbuzz"
 
     } else if (i % 3 === 0){
         console.log("Fizz");
+        txtResult = "Fizz";
         bgClass = "fizz"
     } else if (i % 5 === 0) {
         console.log("Buzz");
+        txtResult = "Buzz";
         bgClass = "buzz"
     } else {
         console.log(i);
+        txtResult = i;
     }
 
     // const boxFizzBuzz = document.createElement("div");
     // boxFizzBuzz = 
-    const boxElem = document.createElement("div");
-    boxElem.innerHTML = i;
-    boxElem.classList.add("box");
-    boxElem.classList.add(bgClass);
-    console.log(boxElem);
-
-    rowElem.append(boxElem);
+    const boxResult = `<div class="box ${bgClass}">${txtResult}</div>`
+    rowElem.innerHTML += boxResult;
 }
