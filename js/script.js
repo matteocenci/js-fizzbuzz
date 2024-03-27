b6dfedbaafb07898a0bf811dca06135dd08089b2
matteocenci <matteocenci01@gmail.com>
2024-03-27 16:36:53 +0100

    feat: FizzBuzz in console working

--- js/script.js
+++ js/script.js
@@ -11,12 +11,25 @@ for (let i = 0; i<=100; i++) {
     let bgClass;
     if(i % 15 === 0) {
         console.log("FizzBuzz");
+        bgClass = "fizzbuzz"
 
     } else if (i % 3 === 0){
         console.log("Fizz");
+        bgClass = "fizz"
     } else if (i % 5 === 0) {
         console.log("Buzz");
+        bgClass = "buzz"
     } else {
         console.log(i);
     }
+
+    // const boxFizzBuzz = document.createElement("div");
+    // boxFizzBuzz = 
+    const boxElem = document.createElement("div");
+    boxElem.innerHTML = i;
+    boxElem.classList.add("box");
+    boxElem.classList.add(bgClass);
+    console.log(boxElem);
+
+    rowElem.append(boxElem);
 }
