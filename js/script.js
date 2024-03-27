3a208bf2d9dc97d3dbb10c5666d14d76e3c32854
matteocenci <matteocenci01@gmail.com>
2024-03-27 16:22:55 +0100

    feat: function if works

--- js/script.js
+++ js/script.js
@@ -7,5 +7,16 @@ console.log(rowElem);
 
 
 for (let i = 0; i<=100; i++) {
-    console.log(i);
+    
+    let bgClass;
+    if(i % 15 === 0) {
+        console.log("FizzBuzz");
+
+    } else if (i % 3 === 0){
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 }
